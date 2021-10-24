f3798ff9849cf98c683c42103a3a8fbb32d74f97
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-23 20:15:42 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
     var TEMPLATEURL = "https://www.timeanddate.com/countdown/generic?iso=[year][month][day]T[hour][minute][second]&p0=2966&msg=[message]&font=sanserif&csz=1#"
-    const currentMonths = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+    var currentMonths = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
         currentDays = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
         currentHours = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
         currentMinutes = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59"],
-        currentDayofWeeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    var currentYear = new Date().getFullYear(),
+        currentDayofWeeks = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+        currentYear = new Date().getFullYear(),
         currentMonth = currentMonths[new Date().getMonth()],
         currentDay = currentDays[new Date().getDate()],
         currentHour = currentHours[new Date().getHours()],
@@ -135,4 +135,3 @@ ${newURL}`)) {
     1. You tried to generate this on a weekend.`)
         }
     }
-    document.body.removeChild(document.getElementById("acgscript"))
