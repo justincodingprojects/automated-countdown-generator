f9bb23444f20ade693034b2fda2f0d92a6fa253a
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-26 12:34:04 -0400

    Update script.js

--- assets/html/js/script.js
+++ assets/html/js/script.js
@@ -1,1 +1,6 @@
+var number = parseInt(document.getElementById("urmom").innerHTML)
 
+setInterval(function() {
+  number = number++
+  document.getElementById("urmom").innerHTML = number.toString()
+}, 1000)
