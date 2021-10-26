e639e1234bbddfe7da7474c9daf87dd42da6edee
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-26 13:02:08 -0400

    Update script.js

--- assets/html/js/script.js
+++ assets/html/js/script.js
@@ -17,8 +17,13 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  if(seconds > 10) {
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
+  } else {
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + "0" + seconds + "s ";
+  }
     
   // If the count down is over, write some text 
   if (distance < 0) {
