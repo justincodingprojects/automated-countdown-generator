81092456a403dca1fb957cfdb4976fa56105b911
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-26 09:33:01 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,2 @@
-alert("Error: Problem Fetching file from url: https://justincodingprojects.github.io/automatedcountdowngenerator/assets/js/script.js")
+alert(`Error: Problem Fetching file from url:
+https://justincodingprojects.github.io/automatedcountdowngenerator/assets/js/script.js`)
