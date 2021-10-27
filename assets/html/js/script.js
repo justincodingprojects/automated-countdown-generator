8408ea7f3a44a5d8fb2dde0d13cf1335c5067f79
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-27 08:03:29 -0400

    Update script.js

--- assets/html/js/script.js
+++ assets/html/js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 27, 2021 08:05:03").getTime();
+var countDownDate = new Date("Oct 27, 2021 08:05:03.5").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,16 +17,16 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  if(seconds < 9) {
+  if(seconds < 10) {
     seconds = "0" + seconds.toString()
   }
-  if(days < 9) {
+  if(days < 10) {
     days = "0" + days.toString()
   }
-  if(hours < 9) {
+  if(hours < 10) {
     hours = "0" + hours.toString()
   }
-  if(minutes < 9) {
+  if(minutes < 10) {
     minutes = "0" + minutes.toString()
   }
     document.getElementById("demo").innerHTML = days + "d " + hours + "h "
