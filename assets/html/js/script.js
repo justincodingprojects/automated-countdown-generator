afab55e01a948a40dc5c82466de1a1ce174c3bac
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-27 18:31:39 -0400

    Update script.js

--- assets/html/js/script.js
+++ assets/html/js/script.js
@@ -35,6 +35,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "TIME'S UP";
   }
 }, 1000);
