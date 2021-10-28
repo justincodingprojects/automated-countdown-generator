6a4448386959ec45fe033fa666fe275cdc48ff27
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-28 12:00:22 -0400

    Update script.js

--- assets/html/js/script.js
+++ assets/html/js/script.js
@@ -1,6 +1,5 @@
 // Set the date we're counting down to
 var countDownDate = new Date(localStorage.getItem("tempValues")).getTime();
-localStorage.removeItem("tempValues")
 
 // Update the count down every 1 second
 var x = setInterval(function() {
