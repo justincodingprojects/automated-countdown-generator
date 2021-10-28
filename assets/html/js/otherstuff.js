43b0d0e2f154d98b68595b6c4690d699e97a4d9f
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-28 11:38:26 -0400

    Update otherstuff.js

--- assets/html/js/otherstuff.js
+++ assets/html/js/otherstuff.js
@@ -1,18 +1,18 @@
 var url = window.location.href;
 alert(window.location.href)
-var setYear = url.substring(url.indexOf('year=') + 6).split(`][month`)[0]
+var setYear = url.substring(url.indexOf('year=') + 5).split(`][month`)[0]
 alert(setYear)
-var setMonth = url.substring(url.indexOf('month=') + 7).split(`][day`)[0]
+var setMonth = url.substring(url.indexOf('month=') + 6).split(`][day`)[0]
 alert(setMonth)
-var setDay = url.substring(url.indexOf('day=') + 5).split(`]T[hour`)[0]
+var setDay = url.substring(url.indexOf('day=') + 4).split(`]T[hour`)[0]
 alert(setDay)
-var setHour = url.substring(url.indexOf('hour=') + 6).split(`][mintue`)[0]
+var setHour = url.substring(url.indexOf('hour=') + 5).split(`][minute`)[0]
 alert(setHour)
-var setMinute = url.substring(url.indexOf('minute=') + 8).split(`][second`)[0]
+var setMinute = url.substring(url.indexOf('minute=') + 7).split(`][second`)[0]
 alert(setMinute)
-var setSecond = url.substring(url.indexOf('second=') + 8).split(`][millisecond`)[0]
+var setSecond = url.substring(url.indexOf('second=') + 7).split(`][millisecond`)[0]
 alert(setSecond)
-var setMillisecond = url.substring(url.indexOf('millisecond=') + 13).split(`&p0=2966`)[0]
+var setMillisecond = url.substring(url.indexOf('millisecond=') + 12).split(`&p0=2966`)[0]
 alert(setMillisecond)
 var egg = new Egg();
 egg
