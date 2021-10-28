e1e131516fadd29a174208e847248a9c4feb3742
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-28 11:55:42 -0400

    Update otherstuff.js

--- assets/html/js/otherstuff.js
+++ assets/html/js/otherstuff.js
@@ -7,6 +7,8 @@ var setMinute = url.substring(url.indexOf('minute=') + 7).split(`][second`)[0]
 var setSecond = url.substring(url.indexOf('second=') + 7).split(`][millisecond`)[0]
 var setMillisecond = url.substring(url.indexOf('millisecond=') + 12).split(`]&p0=2966`)[0]
 
+localStorage.setItem("tempValues", setMonth + " " + setDay + ", " + setYear + " " + setHour = ":" = setMinute + ":" + setSecond + "." + setMillisecond)
+
 var egg = new Egg();
 egg
   .addCode("q", function() {
