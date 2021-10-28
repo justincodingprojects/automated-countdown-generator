682ef112fa85a553d909b478ec3c9231381bd403
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-28 09:50:01 -0400

    Update otherstuff.js

--- assets/html/js/otherstuff.js
+++ assets/html/js/otherstuff.js
@@ -1,3 +1,15 @@
-
+var url = window.location.href;
+alert(window.location.href)
 var setDate = url.substring(url.indexOf('hour=') + 1).split(`
 `)[0]
+     var egg = new Egg();
+egg
+  .addCode("q", function() {
+  if(document.body.style.backgroundColor == "white" || document.body.style.backgroundColor == "") {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("demo").style.color = "white";
+} else {
+    document.body.style.backgroundColor = "white";
+    document.getElementById("demo").style.color = "black";
+}
+  }).listen();
