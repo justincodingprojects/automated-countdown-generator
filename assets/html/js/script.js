beb4d3ffa502b9335700514f063a645cb5a286c9
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-28 08:57:22 -0400

    Update script.js

--- assets/html/js/script.js
+++ assets/html/js/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 28, 2021 09:48").getTime();
+var countDownDate = new Date("Oct 28, 2021 09:47:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
