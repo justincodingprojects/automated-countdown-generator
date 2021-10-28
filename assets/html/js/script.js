44e454b5dd0f48c119112899155ceca1b3c6d985
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-10-28 11:42:49 -0400

    Update script.js

--- assets/html/js/script.js
+++ assets/html/js/script.js
@@ -1,5 +1,13 @@
+var url = window.location.href;
+var setYear = url.substring(url.indexOf('year=') + 5).split(`][month`)[0]
+var setMonth = url.substring(url.indexOf('month=') + 6).split(`][day`)[0]
+var setDay = url.substring(url.indexOf('day=') + 4).split(`]T[hour`)[0]
+var setHour = url.substring(url.indexOf('hour=') + 5).split(`][minute`)[0]
+var setMinute = url.substring(url.indexOf('minute=') + 7).split(`][second`)[0]
+var setSecond = url.substring(url.indexOf('second=') + 7).split(`][millisecond`)[0]
+var setMillisecond = url.substring(url.indexOf('millisecond=') + 12).split(`]&p0=2966`)[0]
 // Set the date we're counting down to
-var countDownDate = new Date().getTime();
+var countDownDate = new Date(`${setMonth} ${setDay}, ${setYear} ${setHour}:${setMinute}:${setSecond}.${setMilisecond}`).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
