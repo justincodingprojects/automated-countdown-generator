2485ac57e376c7c920c8c4433ac880c1fa7d5fac
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-06 10:42:46 -0400

    Create maintenancescript.js

---
+++ assets/html/js/maintenancescript.js
@@ -0,0 +1,8 @@
+var div = document.createElement("div")
+div.setAttribute("class", "topnav")
+document.body.appendChild(div)
+var a = document.createElement("a")
+a.setAttribute("id", "mainbanner")
+a.setAttribute("class", "active")
+document.getElementsByClassName("topnav")[0].appendChild(a)
+document.getElementById("mainbanner").appendChild(document.createTextNode("This website is currently in ongoing maintenance, expect to see some small design changes over the days."))
