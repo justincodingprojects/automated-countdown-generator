2e6647048da8d2d6338440dc6d9b3edd78d3e4ce
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-07 11:32:47 -0500

    Update iframescript.js

--- assets/html/js/iframescript.js
+++ assets/html/js/iframescript.js
@@ -1,7 +1,10 @@
 var isinIFrame = top.location != self.location
   if(isinIFrame == true) {
-    /*document.getElementById("title").style = "text-align: center; font-size: 275%; margin-top: 0px; font-family: 'Roboto', sans-serif; color: white; position: absolute; top: 40%; left: 50%; margin-right: -50%; transform: translate(-40%, -50%);"
+    document.getElementById("title").style = "text-align: center; font-size: 275%; margin-top: 0px; font-family: 'Roboto', sans-serif; color: white; position: absolute; top: 40%; left: 50%; margin-right: -50%; transform: translate(-40%, -50%);"
     document.getElementById("demo").style = "text-align: center; font-size: 275%; margin-top: 0px; font-family: 'Roboto', sans-serif; color: white; position: absolute; top: 65%; left: 50%; margin-right: -50%; transform: translate(-50%, -65%);"
+    setInterval(function() {
+      if(onMaintenance.isOn == true) {
     document.getElementById("mainbanner").style.fontSize = "15px"
-    */
+    }
+    }, 1000)
   }
