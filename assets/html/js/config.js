e50f6623a8c22865cc5a61c5b85e7384a9e62728
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-08 09:34:58 -0500

    Update config.js

--- assets/html/js/config.js
+++ assets/html/js/config.js
@@ -1,22 +1,16 @@
 setTimeout(function() {
     onMaintenance.isOn = true;
       if(top.location != self.location) {
-        var head  = document.getElementsByTagName('head')[0];
         var link  = document.createElement('link');
-        link.id   = cssId;
         link.rel  = 'stylesheet';
         link.type = 'text/css';
         link.href = './css/style-iframe.css';
-        link.media = 'all';
-        head.appendChild(link);
+        document.head.appendChild(link);
       } else {
-        var head  = document.getElementsByTagName('head')[0];
         var link  = document.createElement('link');
-        link.id   = cssId;
         link.rel  = 'stylesheet';
         link.type = 'text/css';
         link.href = './css/style.css';
-        link.media = 'all';
-        head.appendChild(link);
+        document.head.appendChild(link);
       }
     }, 50)
