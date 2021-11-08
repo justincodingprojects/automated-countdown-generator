a44ff4d0eda9486b7f5fee83f8f627887cde95d3
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-08 08:33:01 -0500

    Update config.js

--- assets/html/js/config.js
+++ assets/html/js/config.js
@@ -3,5 +3,7 @@ setTimeout(function() {
       if(top.location != self.location) {
         document.getElementById("title").fontSize = "50px"
         document.getElementById("demo").fontSize = "50px"
+        document.getElementById("demo").top = "63%"
+        document.getElementById("demo").transform = "translate(-50%, -63%)"
       }
     }, 50)
