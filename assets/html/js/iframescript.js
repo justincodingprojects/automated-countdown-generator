90ca05fd746c3f54c50afa034c68fbbed9f404fe
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-08 07:47:58 -0500

    Delete iframescript.js

--- assets/html/js/iframescript.js
+++
@@ -1,6 +0,0 @@
-var isinIFrame = top.location != self.location
-  if(isinIFrame == true) {
-    document.getElementById("title").style = "text-align: center; font-size: 275%; margin-top: 0px; font-family: 'Roboto', sans-serif; color: white; position: absolute; top: 40%; left: 50%; margin-right: -50%; transform: translate(-40%, -50%);"
-    document.getElementById("demo").style = "text-align: center; font-size: 275%; margin-top: 0px; font-family: 'Roboto', sans-serif; color: white; position: absolute; top: 65%; left: 50%; margin-right: -50%; transform: translate(-50%, -65%);"
-    document.body.removeNode(document.getElementsByClassName("topnav")[0])
-  }
