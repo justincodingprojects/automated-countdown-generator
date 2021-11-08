802e9b370f9759f8ca3921b4f31f50606fc74d52
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-08 08:05:31 -0500

    Update config.js

--- assets/html/js/config.js
+++ assets/html/js/config.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     onMaintenance.isOn = true;
       if(top.location != self.location) {
-        document.getElementById("title").style = "text-align: center; font-size: 275%; margin-top: 0px; font-family: 'Roboto', sans-serif; color: white; position: absolute; top: 40%; left: 50%; margin-right: -50%; transform: translate(-40%, -50%);"
-        document.getElementById("demo").style = "text-align: center; font-size: 275%; margin-top: 0px; font-family: 'Roboto', sans-serif; color: white; position: absolute; top: 65%; left: 50%; margin-right: -50%; transform: translate(-50%, -65%);"
+        document.getElementById("title").style = "text-align: center; font-size: 10px; margin-top: 0px; font-family: 'Roboto', sans-serif; color: white; position: absolute; top: 40%; left: 50%; margin-right: -50%; transform: translate(-40%, -50%);"
+        document.getElementById("demo").style = "text-align: center; font-size: 10px; margin-top: 0px; font-family: 'Roboto', sans-serif; color: white; position: absolute; top: 65%; left: 50%; margin-right: -50%; transform: translate(-50%, -65%);"
       }
     }, 50)
