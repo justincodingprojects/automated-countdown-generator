9caa0ce60272666ff03e1c21c0fcb0c21ba199f4
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-08 08:40:27 -0500

    Update config.js

--- assets/html/js/config.js
+++ assets/html/js/config.js
@@ -5,5 +5,6 @@ setTimeout(function() {
         document.getElementById("demo").fontSize = "50px"
         document.getElementById("demo").top = "63%"
         document.getElementById("demo").transform = "translate(-50%, -63%)"
+        alert(document.getElementById("title").fontSize + " " + document.getElementById("demo").fontSize + " " + document.getElementById("demo").top + " " + document.getElementById("demo").transform)
       }
     }, 50)
