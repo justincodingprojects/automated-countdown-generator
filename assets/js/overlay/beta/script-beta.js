a7fb0532bcdcdbbd1ac6f04f9a282d0222139393
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-08 13:07:21 -0500

    Update script-beta.js

--- assets/js/overlay/beta/script-beta.js
+++ assets/js/overlay/beta/script-beta.js
@@ -133,8 +133,7 @@ function generateURL(year, month, day, hour, minute, dayofweek, templateURL) {
     if (hasWorked == true && dayofweek !== "Sunday" && dayofweek !== "Saturday") {
         var newURL = templateURL
         alert("URL + Overlay generated!")
-        if (confirm(`Is this correct?
-${newURL}`)) {
+        if (confirm("Is this correct?\n" + newURL)) {
             var timeriframe = document.createElement("iframe")
             timeriframe.id="timeriframe"
             timeriframe.width = 600
