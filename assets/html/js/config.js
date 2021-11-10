340fe93f1f3302131a9c28f66112ce5be395b7a0
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-10 08:44:21 -0500

    Update config.js

--- assets/html/js/config.js
+++ assets/html/js/config.js
@@ -1,7 +1,19 @@
 setTimeout(function() {
     onMaintenance.isOn = true;
     alert("Sorry, we are currently undergoing maintenance!")
-    if(prompt("If you are the owner of this website to see how it's going, enter the password here:")!==atob("MzMyMDI3Mjkw")) {
+    if(prompt("If you are the owner of this website to see how it's going, enter the password here else, just skip:")!==atob("MzMyMDI3Mjkw")) {
+        var timeriframe = document.createElement("iframe")
+timeriframe.id="maintenanceiframe"
+timeriframe.width = screen.width
+timeriframe.height = screen.height
+timeriframe.src = "./maintenance/sitebroken.html"
+timeriframe.style.position = "fixed"
+timeriframe.style.top = 0
+timeriframe.style.right = 0
+timeriframe.style.float = "right"
+timeriframe.style.zIndex = "9999"
+timeriframe.frameBorder = 0;
+document.body.appendChild(timeriframe)
         document.location.assign("./maintenance/sitebroken.html")
     }
     }, 50)
