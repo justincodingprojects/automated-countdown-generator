943cba95bcbff0326579ef4184ff7d9e921a838f
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-10 11:57:04 -0500

    Update config.js

--- assets/html/js/config.js
+++ assets/html/js/config.js
@@ -1,7 +1,15 @@
 setTimeout(function() {
     onMaintenance.isOn = true;
+    var redirect = true;
+    var redirectPlusPassword = true;
+    if(onMaintenance.isOn) {
+        if(redirect) {
     alert("Sorry, we are currently undergoing maintenance!")
     if(prompt("If you are the owner of this website to see how it's going, enter the password here else, just skip:")!==atob("MzMyMDI3Mjkw")) {
+        if(redirectPlusPassword) {
        window.location.href = "./maintenance/sitebroken.html"
+        }
     }
-    }, 50)
+        }
+    }
+}, 50)
