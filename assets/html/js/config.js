4362137f553ebd5c637e27bb9e521d2a3d1dd3fe
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-10 08:34:03 -0500

    Update config.js

--- assets/html/js/config.js
+++ assets/html/js/config.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     onMaintenance.isOn = true;
     alert("Sorry, we are currently undergoing maintenance!")
-    if(prompt("If you are the owner of this website to see how it's going, enter the password here:")!=="332027290") {
+    if(prompt("If you are the owner of this website to see how it's going, enter the password here:")!==atob("MzMyMDI3Mjkw")) {
         document.location.href="./maintenance/sitebroken.html"
     }
     }, 50)
