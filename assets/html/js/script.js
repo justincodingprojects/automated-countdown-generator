12182402b3e258d8d6b048715ad22dc596e5b7f7
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-16 08:12:56 -0500

    Update script.js

--- assets/html/js/script.js
+++ assets/html/js/script.js
@@ -59,7 +59,6 @@ var x = setInterval(function() {
   + minutes + "m " + seconds + "s"
       document.body.appendChild(pElement)
       $("#demo").fadeOut(500)
-      setTimeout(function() {
          pElement.style.opacity = 0.1
          setTimeout(function() {
          pElement.style.opacity = 0.2
@@ -88,7 +87,6 @@ var x = setInterval(function() {
       }, 50)
       }, 50)
       }, 50)
-      }, 100)
       setTimeout(function() {
       document.body.removeChild(document.getElementById("demo"))
       document.getElementById("demo1").id = "demo"
