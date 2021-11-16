9d12b55a97ffe6bbc44dc99b61c966955806a6eb
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-16 10:54:33 -0500

    Update maintenancescript.js

--- assets/html/js/maintenancescript.js
+++ assets/html/js/maintenancescript.js
@@ -29,7 +29,7 @@ var a = document.createElement("a")
 a.setAttribute("id", "mainbanner")
 a.setAttribute("class", "active")
 document.getElementsByClassName("topnav")[0].appendChild(a)
-document.getElementById("mainbanner").appendChild(document.createTextNode("This website is currently in ongoing maintenance, expect to see some small design changes over the days."))
+document.getElementById("mainbanner").appendChild(/*document.createTextNode("This website is currently in ongoing maintenance, expect to see some small design changes over the days.")*/document.createTextNode("If you're seeing any weird text below the countdown or this banner going weird, I'm working on the site."))
 }, 50)
 }
   } else if(bool == false) {
