c3056a8fcc46c4055acd9084e20648d66eec19b8
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-18 13:01:16 -0500

    Update addfaderemove.js

--- assets/html/js/addfaderemove.js
+++ assets/html/js/addfaderemove.js
@@ -1,4 +1,16 @@
 var clicks = 0;
 var egg = new Egg("f,a,d,e", function() {
+  if(clicks == 2) {
   alert("you are a bruh")
+  }
 }).listen();
+function addClickTitle() {
+  if(clicks == 0) {
+    clicks = 1
+  }
+}
+function addClickDemo() {
+  if(clicks == 1) {
+    clicks = 2
+  }
+}
