bea43efbaf74b2a1baf31c0c3ea7008b51995791
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-18 07:21:03 -0500

    Update addfaderemove.js

--- assets/html/js/addfaderemove.js
+++ assets/html/js/addfaderemove.js
@@ -1,11 +1,1 @@
-var container = document.getElementById('demo1')
-var timesClicked = 0
-document.addEventListener('click', function( event ) {
-  if (container !== event.target && !container.contains(event.target)) {    
-    timesClicked++
-    if(timesClicked == 5) {
-    alert("you pressed it five times")
-      timesClicked = 0
-    }
-  }
-})
+alert("if you see this, then the script adding worked.")
