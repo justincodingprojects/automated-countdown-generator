03546ef26b25605021805f8f15086c764c8b31b5
justincodingprojects <90016710+justincodingprojects@users.noreply.github.com>
2021-11-18 13:06:50 -0500

    Update addfaderemove.js

--- assets/html/js/addfaderemove.js
+++ assets/html/js/addfaderemove.js
@@ -1,16 +1,19 @@
 var clicks = 0;
-var egg = new Egg("f,a,d,e", function() {
-  if(clicks == 2) {
-  alert("you are a bruh")
+var egg = new Egg("f", function() {
+  if(clicks == 3) {
+    document.getElementById("fadetolinear").style.display = ""
+    document.getElementById("fadetolinear2").style.display = ""
   }
 }).listen();
 function addClickTitle() {
   if(clicks == 0) {
     clicks = 1
+  } else if(clicks == 1) {
+    clicks = 2
   }
 }
 function addClickDemo() {
-  if(clicks == 1) {
-    clicks = 2
+  if(clicks == 2) {
+    clicks = 3
   }
 }
