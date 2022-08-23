f3639a601711cf898095d3b67ce65bbfd8012bb3
Justin Programming Skills <90016710+justincodingprojects@users.noreply.github.com>
2022-08-23 07:32:20 -0400

    Update index-script.js

--- index-script.js
+++ index-script.js
@@ -474,7 +474,7 @@
                         }
                     }
                     document.getElementById("svg3").onclick = function() {
-                        if (parseInt(new Array("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31")[new Date().getDate()]) == 4) {
+                        if (parseInt(new Array("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31")[new Date().getDate()]) == 23) {
                             timefortest()
                         } else {
                             var TEMPLATEURL = window.location.href + "?month=[month]&day=[day]&year=[year]&hour=[hour]&minute=[minute]&second=[second]&millisecond=00&message=[message]"
