457aacf81271891aa668e194662cebfa3b8e7e6d
Justin Programming Skills <90016710+justincodingprojects@users.noreply.github.com>
2023-01-25 15:54:11 -0500

    Fixing More Stuff

--- countdown-script-main.js
+++ countdown-script-main.js
@@ -1,9 +1,9 @@
 var urlParams = window.location.href
-Date.prototype.today = function() {
+Date.prototype.today = function () {
     return (((this.getMonth() + 1) < 10) ? "0" : "") + (this.getMonth() + 1) + "/" + ((this.getDate() < 10) ? "0" : "") + this.getDate() + "/" + this.getFullYear();
 }
 
-Date.prototype.timeNow = function() {
+Date.prototype.timeNow = function () {
     return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
 }
 if (urlParams.indexOf("?month=") != -1 &&
@@ -20,58 +20,110 @@ if (urlParams.indexOf("?month=") != -1 &&
         ":" + urlParams.substring(urlParams.indexOf('minute=') + 7).split(`&second=`)[0] +
         ":" + urlParams.substring(urlParams.indexOf('second=') + 7).split(`&millisecond=`)[0] +
         "." + urlParams.substring(urlParams.indexOf('millisecond=') + 12).split(`&message=`)[0]).getTime();
+    const fetchSampleImplementation = async () => {
+        const requestDate = new Date();
+
+        var xmlHttp = new XMLHttpRequest()
+        xmlHttp.open('HEAD', window.location.href.toString(), false);
+        xmlHttp.setRequestHeader("Content-Type", "text/html");
+        xmlHttp.send('');
+        var statusText = xmlHttp.status
+        var ok = (statusText == 200) ? true : false
+
+        if (!ok) {
+            throw new Error(`Bad date sample from server: ${statusText}`);
+        }
+
+        return {
+            requestDate,
+            responseDate: new Date(),
+            serverDate: new Date(xmlHttp.getResponseHeader("Date")),
+        };
+    };
+
+    const getServerDate = async (
+        { fetchSample } = { fetchSample: fetchSampleImplementation }
+    ) => {
+        let best = { uncertainty: Number.MAX_VALUE };
+
+        // Fetch 10 samples to increase the chance of getting one with low
+        // uncertainty.
+        for (let index = 0; index < 10; index++) {
+            try {
+                const { requestDate, responseDate, serverDate } = await fetchSample();
+
+                // We don't get milliseconds back from the Date header so there's
+                // uncertainty of at least half a second in either direction.
+                const uncertainty = (responseDate - requestDate) / 2 + 500;
+
+                if (uncertainty < best.uncertainty) {
+                    const date = new Date(serverDate.getTime() + 500);
+
+                    best = {
+                        date,
+                        offset: date - responseDate,
+                        uncertainty,
+                    };
+                }
+            } catch (exception) {
+                console.warn(exception);
+            }
+        }
+
+        return best;
+    };
     if ("wakeLock" in navigator) {
         let wakeLock = null;
         async function start() {
             wakeLock = await navigator.wakeLock.request('screen');
         }
-        document.addEventListener('visibilitychange', async() => {
+        document.addEventListener('visibilitychange', async () => {
             if (wakeLock !== null && document.visibilityState === 'visible') {
                 wakeLock = await navigator.wakeLock.request('screen');
             }
         });
         start();
     }
-    var egg = new Egg("esc", function() {
+    var egg = new Egg("esc", function () {
         window.top.document.body.removeChild(window.parent.document.getElementById("iframeModal"))
     }).listen();
     let lastOffsetTime = {};
 
-      const synchronize = async () => {
+    const synchronize = async () => {
         lastOffsetTime = await getServerDate();
-      };
-      synchronize();
-      setInterval(synchronize, 60 * 1000);
-    var x = setInterval(function() {
-        const {date, offset, uncertainty } = lastOffsetTime;
+    };
+    synchronize();
+    setInterval(synchronize, 60 * 1000);
+    var x = setInterval(function () {
+        const { date, offset, uncertainty } = lastOffsetTime;
         var now = new Date(Date.now() + offset).getTime()
 
         var distance = countDownDate - now;
         if (decodeURIComponent(urlParams.substring(urlParams.indexOf('message=') + 8)) !== document.getElementById("title").innerHTML) {
-            if(decodeURIComponent(urlParams.substring(urlParams.indexOf('message=') + 8)) !== document.title.innerHTML) {
-            document.getElementById("title").innerHTML = decodeURIComponent(urlParams.substring(urlParams.indexOf('message=') + 8))
+            if (decodeURIComponent(urlParams.substring(urlParams.indexOf('message=') + 8)) !== document.title.innerHTML) {
+                document.getElementById("title").innerHTML = decodeURIComponent(urlParams.substring(urlParams.indexOf('message=') + 8))
             }
         } else {
-            if("Countdown Timer" !== document.getElementById("title").innerHTML) {
-            document.getElementById("title").innerHTML = "Countdown Timer"
+            if ("Countdown Timer" !== document.getElementById("title").innerHTML) {
+                document.getElementById("title").innerHTML = "Countdown Timer"
             }
         }
         if (decodeURIComponent(urlParams.substring(urlParams.indexOf('message=') + 8)) != "") {
             document.title = decodeURIComponent(urlParams.substring(urlParams.indexOf('message=') + 8)) + " - Auto. Countdown Generator | Justin Coding Projects"
         }
         document.getElementById("footera").innerHTML = "Timer until " + new Date(urlParams.substring(urlParams.indexOf('month=') + 6).split(`&day=`)[0] +
-                " " + urlParams.substring(urlParams.indexOf('day=') + 4).split(`&year=`)[0] +
-                " " + urlParams.substring(urlParams.indexOf('year=') + 5).split(`&hour=`)[0]).toLocaleDateString("default", {
+            " " + urlParams.substring(urlParams.indexOf('day=') + 4).split(`&year=`)[0] +
+            " " + urlParams.substring(urlParams.indexOf('year=') + 5).split(`&hour=`)[0]).toLocaleDateString("default", {
                 weekday: 'long'
             }) + ", " + new Date(urlParams.substring(urlParams.indexOf('month=') + 6).split(`&day=`)[0] +
                 " " + urlParams.substring(urlParams.indexOf('day=') + 4).split(`&year=`)[0] +
                 " " + urlParams.substring(urlParams.indexOf('year=') + 5).split(`&hour=`)[0]).toLocaleDateString("default", {
-                month: 'long'
-            }) + " " + new Date(urlParams.substring(urlParams.indexOf('month=') + 6).split(`&day=`)[0] +
-                " " + urlParams.substring(urlParams.indexOf('day=') + 4).split(`&year=`)[0] +
-                " " + urlParams.substring(urlParams.indexOf('year=') + 5).split(`&hour=`)[0]).getDate() + ", " + new Date(urlParams.substring(urlParams.indexOf('month=') + 6).split(`&day=`)[0] +
-                " " + urlParams.substring(urlParams.indexOf('day=') + 4).split(`&year=`)[0] +
-                " " + urlParams.substring(urlParams.indexOf('year=') + 5).split(`&hour=`)[0]).getFullYear() + " at " + ((parseInt(urlParams.substring(urlParams.indexOf('hour=') + 5).split(`&minute=`)[0]) + 11) % 12 + 1) +
+                    month: 'long'
+                }) + " " + new Date(urlParams.substring(urlParams.indexOf('month=') + 6).split(`&day=`)[0] +
+                    " " + urlParams.substring(urlParams.indexOf('day=') + 4).split(`&year=`)[0] +
+                    " " + urlParams.substring(urlParams.indexOf('year=') + 5).split(`&hour=`)[0]).getDate() + ", " + new Date(urlParams.substring(urlParams.indexOf('month=') + 6).split(`&day=`)[0] +
+                        " " + urlParams.substring(urlParams.indexOf('day=') + 4).split(`&year=`)[0] +
+                        " " + urlParams.substring(urlParams.indexOf('year=') + 5).split(`&hour=`)[0]).getFullYear() + " at " + ((parseInt(urlParams.substring(urlParams.indexOf('hour=') + 5).split(`&minute=`)[0]) + 11) % 12 + 1) +
             ":" + urlParams.substring(urlParams.indexOf('minute=') + 7).split(`&second=`)[0] + " " + ((parseInt(urlParams.substring(urlParams.indexOf('hour=') + 5).split(`&minute=`)[0]) >= 12) ? "PM" : "AM");
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -103,7 +155,7 @@ if (urlParams.indexOf("?month=") != -1 &&
         }
         if (distance < 0) {
             clearInterval(x);
-            setTimeout(function() {
+            setTimeout(function () {
                 document.getElementById("demo").innerHTML = "Countdown Ended"
                 wakeLock.release()
                     .then(() => {
@@ -116,57 +168,3 @@ if (urlParams.indexOf("?month=") != -1 &&
 } else {
     alert("Incorrect parameters.")
 }
-
-/* Server-Date Communication */
-const fetchSampleImplementation = async () => {
-    const requestDate = new Date();
-  
-    var xmlHttp = new XMLHttpRequest()
-    xmlHttp.open('HEAD',window.location.href.toString(),false);
-    xmlHttp.setRequestHeader("Content-Type", "text/html");
-    xmlHttp.send('');
-    var statusText = xmlHttp.status
-    var ok = (statusText == 200) ? true : false
-  
-    if (!ok) {
-      throw new Error(`Bad date sample from server: ${statusText}`);
-    }
-  
-    return {
-      requestDate,
-      responseDate: new Date(),
-      serverDate: new Date(xmlHttp.getResponseHeader("Date")),
-    };
-  };
-  
-  const getServerDate = async (
-    { fetchSample } = { fetchSample: fetchSampleImplementation }
-  ) => {
-    let best = { uncertainty: Number.MAX_VALUE };
-  
-    // Fetch 10 samples to increase the chance of getting one with low
-    // uncertainty.
-    for (let index = 0; index < 10; index++) {
-      try {
-        const { requestDate, responseDate, serverDate } = await fetchSample();
-  
-        // We don't get milliseconds back from the Date header so there's
-        // uncertainty of at least half a second in either direction.
-        const uncertainty = (responseDate - requestDate) / 2 + 500;
-  
-        if (uncertainty < best.uncertainty) {
-          const date = new Date(serverDate.getTime() + 500);
-  
-          best = {
-            date,
-            offset: date - responseDate,
-            uncertainty,
-          };
-        }
-      } catch (exception) {
-        console.warn(exception);
-      }
-    }
-  
-    return best;
-  };
