cbd59c6376cf075b58d9b39e2d9ac046ce1ade11
Justin Programming Skills <90016710+justincodingprojects@users.noreply.github.com>
2023-01-25 20:05:25 -0500

    Polishing More Stuff

--- countdown-script-main.js
+++ countdown-script-main.js
@@ -20,58 +20,6 @@ if (urlParams.indexOf("?month=") != -1 &&
         ":" + urlParams.substring(urlParams.indexOf('minute=') + 7).split(`&second=`)[0] +
         ":" + urlParams.substring(urlParams.indexOf('second=') + 7).split(`&millisecond=`)[0] +
         "." + urlParams.substring(urlParams.indexOf('millisecond=') + 12).split(`&message=`)[0]).getTime();
-    const fetchSampleImplementation = async () => {
-        const requestDate = new Date();
-
-        var xmlHttp = new XMLHttpRequest()
-        xmlHttp.open('HEAD', window.location.href.toString(), false);
-        xmlHttp.setRequestHeader("Content-Type", "text/html");
-        xmlHttp.send('');
-        var statusText = xmlHttp.status
-        var ok = (statusText == 200) ? true : false
-
-        if (!ok) {
-            throw new Error(`Bad date sample from server: ${statusText}`);
-        }
-
-        return {
-            requestDate,
-            responseDate: new Date(),
-            serverDate: new Date(xmlHttp.getResponseHeader("Date")),
-        };
-    };
-
-    const getServerDate = async (
-        { fetchSample } = { fetchSample: fetchSampleImplementation }
-    ) => {
-        let best = { uncertainty: Number.MAX_VALUE };
-
-        // Fetch 10 samples to increase the chance of getting one with low
-        // uncertainty.
-        for (let index = 0; index < 10; index++) {
-            try {
-                const { requestDate, responseDate, serverDate } = await fetchSample();
-
-                // We don't get milliseconds back from the Date header so there's
-                // uncertainty of at least half a second in either direction.
-                const uncertainty = (responseDate - requestDate) / 2 + 500;
-
-                if (uncertainty < best.uncertainty) {
-                    const date = new Date(serverDate.getTime() + 500);
-
-                    best = {
-                        date,
-                        offset: date - responseDate,
-                        uncertainty,
-                    };
-                }
-            } catch (exception) {
-                console.warn(exception);
-            }
-        }
-
-        return best;
-    };
     let wakeLock = null;
     if ("wakeLock" in navigator) {
         async function start() {
@@ -87,15 +35,9 @@ if (urlParams.indexOf("?month=") != -1 &&
     var egg = new Egg("esc", function () {
         window.top.document.body.removeChild(window.parent.document.getElementById("iframeModal"))
     }).listen();
-    let lastOffsetTime = {};
 
-    const synchronize = async () => {
-        lastOffsetTime = await getServerDate();
-    };
-    synchronize();
-    setInterval(synchronize, 1000);
     var x = setInterval(function () {
-        var now = new Date(Date.now() + lastOffsetTime.offset).getTime()
+        var now = ServerDate.now()
 
         var distance = countDownDate - now;
         if (decodeURIComponent(urlParams.substring(urlParams.indexOf('message=') + 8)) !== "") {
